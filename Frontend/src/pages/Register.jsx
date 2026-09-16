219b4378cdd86ac3c3389c1e536258cdc9507a51
HumairaMahi-03 <humairamumtahinmahi@gmail.com>
2026-09-16 10:38:49 +0600

    initiaized frontend

---
+++ Frontend/src/pages/Register.jsx
@@ -0,0 +1,76 @@
+import { useState } from "react";
+import api from "../services/api";
+
+function Register() {
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleRegister = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await api.post("/auth/register", {
+                name,
+                email,
+                password
+            });
+
+            console.log(response.data);
+
+            alert("Registration successful!");
+
+        } catch (error) {
+            console.log(error);
+
+            alert(
+                error.response?.data?.message ||
+                "Registration failed"
+            );
+        }
+    };
+
+    return (
+        <div>
+            <h1>Create Account</h1>
+
+            <form onSubmit={handleRegister}>
+
+                <input
+                    type="text"
+                    placeholder="Name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                />
+
+                <br /><br />
+
+                <input
+                    type="email"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+
+                <br /><br />
+
+                <input
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+
+                <br /><br />
+
+                <button type="submit">
+                    Register
+                </button>
+
+            </form>
+        </div>
+    );
+}
+
+export default Register;
